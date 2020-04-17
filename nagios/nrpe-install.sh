9254abe46e394ac49600a5c88150a1e768d5659d
Eli <vincent.brown@seattlecolleges.edu>
2020-04-16 17:15:58 -0700

    Update and rename nrpe_install.sh to nrpe-install.sh

--- nagios/nrpe-install.sh
+++ nagios/nrpe-install.sh
@@ -3,11 +3,9 @@ yum install -y nagios-plugins nrpe nagios-plugins-load nagios-plugins-ping nagio
 # BUG:https://osric.com/chris/accidental-developer/2016/12/missing-nagios-plugins-in-centos-7/ (nrpe plugins have been packaged seperately and don't install with nagios-plugins-all)
 # BUG #2 https://cloudwafer.com/blog/installing-nagios-agent-npre-on-centos/ (the nrpe config is commented out and checks are not defined)
 # Use sed statments to uncomment NRPE config and add the appropiate flags
-# add in command[check_mem]=/usr/lib64/nagios/plugins/check_mem.sh
+# add in command[check_mem]=/usr/lib64/nagios/plugins/check-memory.sh
 # Install custom mem monitor
-wget -O /usr/lib64/nagios/plugins/check_mem.sh https://raw.githubusercontent.com/Eli-Brown/NTI-320-Atom-Smasher/master/nagios/check-memory.sh
-                                               
-chmod +x /usr/lib64/nagios/plugins/check_mem.sh
+wget -O /usr/lib64/nagios/plugins/check-memory.sh https://raw.githubusercontent.com/Eli-Brown/NTI-320-Atom-Smasher/master/nagios/check-memory.sh                                             
 systemctl enable nrpe
 systemctl start nrpe
 sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, 10.142.0.15/g' /etc/nagios/nrpe.cfg
