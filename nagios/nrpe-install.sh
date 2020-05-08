2ddc87ba9e0824352aaacbdbe913018b5da03c47
Eli <vincent.brown@seattlecolleges.edu>
2020-05-07 18:52:20 -0700

    replace the sed line#14  with echo ""
    line on line 14 with this:
    
    echo "command[check_disk]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/disk" >> /etc/nagios/nrpe.cfg

--- nagios/nrpe-install.sh
+++ nagios/nrpe-install.sh
@@ -11,8 +11,10 @@ systemctl enable nrpe
 systemctl start nrpe
 sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, 10.142.0.15/g' /etc/nagios/nrpe.cfg
 # sed -i "s,command[check_hda1]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/hda1,command[check_disk]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/sda1,g" /etc/nagios/nrpe.cfg
-sed -i "s,command[check_hda1], command[check_disk],g" /etc/nagios/nrpe.cfg
-echo "command[check_mem]=/usr/lib64/nagios/plugins/check_mem -w 80 -c 90" >> /etc/nagios/nrpe.cfg
+echo "command[check_disk]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/disk" >> /etc/nagios/nrpe.cfg
+ 
+#sed -i "s,command[check_hda1], command[check_disk],g" /etc/nagios/nrpe.cfg
+echo "command[check_mem]=/usr/lib64/nagios/plugins/check_mem -w 80 -c 90" >> /etc/nagios/nrpe.cfg   #removed for resubmit
 
 systemctl restart nrpe
 
