293409559ca7262145194f46d249e3430c484f02
Eli <vincent.brown@seattlecolleges.edu>
2020-06-04 18:35:33 -0700

    Update scp_to_nagios.sh

--- scp_to_nagios.sh
+++ scp_to_nagios.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 bash generate_config.sh $1 $2
-gcloud compute scp --zone us-central1-a $1.cfg nicolebade@nagios:/etc/nagios/servers
+# server NAME and server IP
+gcloud compute scp --zone us-central1-a $1.cfg elibrown.scc@nagios:/etc/nagios/servers
 # Note: I had to add user nicolebade to group nagios using usermod -a -G nagios nicolebade
 # I also had to chmod 777 /etc/nagios/servers
-gcloud compute ssh --zone us-central1-a nicolebade@nagios --command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'
+gcloud compute ssh --zone us-central1-a elibrown.scc@nagios --command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'
