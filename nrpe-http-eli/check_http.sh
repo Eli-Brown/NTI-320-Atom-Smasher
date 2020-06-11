54d7c3938e3ea80dbb17f765193d117abf6bdc31
Eli <vincent.brown@seattlecolleges.edu>
2020-06-11 13:06:33 -0700

    Update check_http.sh

--- nrpe-http-eli/check_http.sh
+++ nrpe-http-eli/check_http.sh
@@ -17,14 +17,18 @@ http_code ="100" ="HTTP Service error"
 http_HTTP-Status-Response=$(curl -s -o http_HTTP_Status_Response.txt -w "%{http_code}" http://127.0.0.1/nagios/
 if [  $ http_HTTP_Status_Response != "200" ]; then
 	echo "No HTTP Service error"
+		exit 200;
 	
-if [  $ http_HTTP_Status_Response != "100" ]; then
-	echo "HTTP Service error"
+	elif [  $ http_HTTP_Status_Response != "100" ]; then
+		echo "HTTP Service error"
+		exit 100;
 	
-if [  $ http_HTTP_Status_Response != "0" ]; then
-	echo "**Inactive HTTP Service**"
+	elif [  $ http_HTTP_Status_Response != "0" ]; then
+		echo "**Inactive HTTP Service**"
+		exit 0;
    	
-    echo "Server returned:"
-    cat HTTP-Status-Response.txt    
+else
+    echo "Server returned:"    
     echo  "%{http_code}"
+    exit;
 fi
