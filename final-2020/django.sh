161f4aff03b23d81e260068b8078d69073865e7f
Eli <vincent.brown@seattlecolleges.edu>
2020-06-18 10:59:45 -0700

    Create django.sh

---
+++ final-2020/django.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done
+echo "[nti-310-epel]
+name=NTI310 EPEL
+baseurl=http://104.197.59.12/epel
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+echo "[nti-310-base]
+name=NTI310 BASE
+baseurl=http://104.197.59.12/base
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+echo "[nti-320]
+name=Extra Packages for Centos from NTI-320 7 - $basearch
+#baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
+# Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
+baseurl=http://10.128.0.7/centos/7/extras/x86_64/Packages/
+enabled=1
+gpgcheck=0
+" > /etc/yum.repos.d/NTI-320.repo
+
+yum -y install python-pip python-devel gcc postgresql-devel postgresql-contrib
+pip install --upgrade pip
+pip install virtualenv
+mkdir /opt/nti310
+cd /opt/nti310
+virtualenv nti310env
+source nti310env/bin/activate
+pip install django psycopg2
+django-admin.py startproject nti310 .
+perl -i -0pe "BEGIN{undef $/;} s/        'ENGINE':.*db.sqlite3'\),/        'ENGINE': 'django.db.backends.postgresql_psycopg2',\n        'NAME': 'nti310',\n        'USER': 'nti310user',\n        'PASSWORD': 'password',\n        'HOST': 'postgres',\n        'PORT': '5432',/smg" /opt/nti310/nti310/settings.py
+python manage.py startapp Cars
+echo "class Specs(models.Model):
+    name = models.CharField(max_length = 20)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    weight = models.PositiveIntegerField()" >> Cars/models.py
+    
+sed -i "40i \ \ \ \ 'Cars'," nti310/settings.py
+python manage.py makemigrations Cars
+python manage.py migrate Cars
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@newproject.com','NTI300NTI300')" | python manage.py shell
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
+echo "*.info;mail.none;authpriv.none;cron.none   @rsyslog" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+
+yum install -y nagios-plugins nrpe nagios-plugins-load nagios-plugins-ping nagios-plugins-disk nagios-plugins-http nagios-plugins-procs nagios-plugins-all wget
+wget -O /usr/lib64/nagios/plugins/check_mem.sh https://raw.githubusercontent.com/nic-instruction/hello-nti-320/master/check_mem.sh
+chmod +x /usr/lib64/nagios/plugins/check_mem.sh
+systemctl enable nrpe
+systemctl start nrpe
+sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, 10.128.0.2/g' /etc/nagios/nrpe.cfg
+echo "command[check_disk]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/disk" >> /etc/nagios/nrpe.cfg
+echo "command[check_mem]=/usr/lib64/nagios/plugins/check_mem.sh -w 80 -c 90" >> /etc/nagios/nrpe.cfg
+systemctl restart nrpe
