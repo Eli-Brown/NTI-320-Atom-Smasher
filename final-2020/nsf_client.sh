9e75e0225ec49289f2b5a215794a08c57297066a
Eli <vincent.brown@seattlecolleges.edu>
2020-06-18 11:49:05 -0700

    IP=10.128.0.19
    IP=10.128.0.19

--- final-2020/nsf_client.sh
+++ final-2020/nsf_client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update
-apt install -y nfs-client
-showmount -e 10.128.15.6
+apt install -y nfs-client httpd
+showmount -e 10.128.0.19
 mkdir /mnt/test
-echo "10.128.15.6:/var/nfsshare/testing     /mnt/test     nfs     defaults 0 0" >> /etc/fstab
+echo "10.128.0.19:/var/nfsshare/testing     /mnt/test     nfs     defaults 0 0" >> /etc/fstab
 mount -a
