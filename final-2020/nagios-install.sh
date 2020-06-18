cf8e794ddbf098c2a75d3e9b2b2f513339463115
Eli <vincent.brown@seattlecolleges.edu>
2020-06-18 16:49:07 -0700

    Update nagios-install.sh

--- final-2020/nagios-install.sh
+++ final-2020/nagios-install.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done
+
+
+echo "[nti-310-epel]
+name=NTI310 EPEL
+baseurl=http://34.71.91.10/epel
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+echo "[nti-310-base]
+name=NTI310 BASE
+baseurl=http://34.71.91.10/base
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+echo "[nti-310-extras]
+name=NTI310 EXTRAS
+baseurl=http://34.71.91.10/extras/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+echo "[nti-310-updates]
+name=NTI310 UPDATES
+baseurl=http://34.71.91.10/updates/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+
 yum -y install nagios              #Install, start and enable nagios
 systemctl enable nagios
 systemctl start nagios
