2fac95346c1f08a8c3d74ac3aefb7bac04b8f7f4
Eli <vincent.brown@seattlecolleges.edu>
2020-06-17 18:26:04 -0700

    Update nrpe-install.sh

--- nagios/nrpe-install.sh
+++ nagios/nrpe-install.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 yum install -y nagios-plugins nrpe nagios-plugins-load nagios-plugins-ping nagios-plugins-disk nagios-plugins-http nagios-plugins-procs wget yum -y install nagios-plugins-all
+#!/bin/bash
+# This should be the finishing script for a micro with a 50G hard drive
 
+yum -y install rpm-build make gcc git                                         # install rpm tools, compiling tools and source tools
+mkdir -p /root/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}                      # create the rpmbuild dirrectory structure 
+                                                                              # (the docs say this happens by default, this is incorrect on centos 7)
+cd ~/
+echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros                         # Set the rpmbuild path in an .rpmmacros file
+cd ~/rpmbuild/SOURCES
+git clone https://github.com/NagiosEnterprises/nrpe.git                       # Get the nrpe source from github
+git clone https://github.com/NagiosEnterprises/nagioscore.git                 # Get the nagios source from github
+
+tar -czvf nrpe-3.1.tar.gz /root/rpmbuild/SOURCES/nrpe                         # Tar up the source (needed to create an RPM)
+tar -czvf nagioscore-4.3.1.tar.gz /root/rpmbuild/SOURCES/nagioscore           # Tar up the source (needed to create an RPM)
+
+mv nagioscore nagioscore-4.3.1                                                # Clean up our source dir by making a dirrectory
+mv nrpe nrpe-3.1                                                              # structure that will allow us to have multiple
+mkdir nagioscore                                                              # versions of our source code
+mkdir nrpe
+mv nagioscore-4.3.1 nagioscore
+mv nrpe-3.1 nrpe
+
+cd ../SPECS                                                                   # head to the SPECS directory
+cp /usr/share/vim/vimfiles/template.spec .                         
 
 # BUG:https://osric.com/chris/accidental-developer/2016/12/missing-nagios-plugins-in-centos-7/ (nrpe plugins have been packaged seperately and don't install with nagios-plugins-all)
 # BUG #2 https://cloudwafer.com/blog/installing-nagios-agent-npre-on-centos/ (the nrpe config is commented out and checks are not defined)
