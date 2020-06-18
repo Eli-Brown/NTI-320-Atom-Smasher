dd62d48775314b5eee0f156932ff41748da4a5cf
Eli <vincent.brown@seattlecolleges.edu>
2020-06-18 16:22:35 -0700

    Update nfs-server.sh

--- final-2020/nfs-server.sh
+++ final-2020/nfs-server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done
+
 echo "[nti-310-epel]
 name=NTI310 EPEL
 baseurl=http://104.197.59.12/epel
@@ -11,14 +12,16 @@ name=NTI310 BASE
 baseurl=http://104.197.59.12/base
 gpgcheck=0
 enabled=1" >> /etc/yum.repos.d/local-repo.repo
-echo "[nti-320]
-name=Extra Packages for Centos from NTI-320 7 - $basearch
-#baseurl=http://download.fedoraproject.org/pub/epel/7/$basearch <- example epel repo
-# Note, this is putting repodata at packages instead of 7 and our path is a hack around that.
-baseurl=http://10.128.0.7/centos/7/extras/x86_64/Packages/
-enabled=1
+echo "[nti-310-extras]
+name=NTI310 EXTRAS
+baseurl=http://104.197.59.12/extras/
 gpgcheck=0
-" > /etc/yum.repos.d/NTI-320.repo
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+echo "[nti-310-updates]
+name=NTI310 UPDATES
+baseurl=http://104.197.59.12/updates/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
 
 yum -y install nfs-utils net-tools
 mkdir /var/nfsshare
