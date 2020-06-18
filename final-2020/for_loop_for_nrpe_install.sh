5ed41f71ae056f3c22c65d58af86c73af559051d
Eli <vincent.brown@seattlecolleges.edu>
2020-06-18 12:48:54 -0700

    where does this need to go
    where does this need to go

---
+++ final-2020/for_loop_for_nrpe_install.sh
@@ -0,0 +1,3 @@
+for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios );  do 
+    gcloud compute ssh --zone us-central1-a elibrown_scc@$servername --command='sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/nic-instruction/NTI-320/master/nagios/nrpe_install.sh && sudo bash nrpe_install.sh'
+done
