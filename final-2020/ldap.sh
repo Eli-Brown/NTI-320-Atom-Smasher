de32998dc856cd8bbfdbd88dafb00801b7864047
Eli <vincent.brown@seattlecolleges.edu>
2020-06-18 19:14:28 -0700

    Update ldap.sh

--- final-2020/ldap.sh
+++ final-2020/ldap.sh
@@ -9,31 +9,26 @@
 
 for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done
 
-
 echo "[nti-310-epel]
 name=NTI310 EPEL
-baseurl=http://34.71.91.10/epel
+baseurl=http://104.197.59.12/epel
 gpgcheck=0
 enabled=1" >> /etc/yum.repos.d/local-repo.repo
-
 echo "[nti-310-base]
 name=NTI310 BASE
-baseurl=http://34.71.91.10/base
+baseurl=http://104.197.59.12/base
 gpgcheck=0
 enabled=1" >> /etc/yum.repos.d/local-repo.repo
-
 echo "[nti-310-extras]
 name=NTI310 EXTRAS
-baseurl=http://34.71.91.10/extras/
+baseurl=http://104.197.59.12/extras/
 gpgcheck=0
 enabled=1" >> /etc/yum.repos.d/local-repo.repo
-
 echo "[nti-310-updates]
 name=NTI310 UPDATES
-baseurl=http://34.71.91.10/updates/
+baseurl=http://104.197.59.12/updates/
 gpgcheck=0
 enabled=1" >> /etc/yum.repos.d/local-repo.repo
-
 
 
 yum install -y git
